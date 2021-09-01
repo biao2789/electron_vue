3141268b90bd8eda09b2209032441137565367f8
biao2789 <357390624@qq.com>
2021-09-01 15:07:14 +0800

    first time commit ,and can run and build

--- src/renderer/utils/request.js
+++ src/renderer/utils/request.js
@@ -11,7 +11,8 @@ const service = axios.create({
 // request拦截器
 service.interceptors.request.use(config => {
   if (store.getters.token) {
-    config.headers['X-Token'] = store.getters.token// 让每个请求携带自定义token 请根据实际情况自行修改
+    config.headers.Authorization = `Bearer ${store.getters.token}`
+    // config.headers['X-Token'] = store.getters.token// 让每个请求携带自定义token 请根据实际情况自行修改
   }
   return config
 }, error => {
